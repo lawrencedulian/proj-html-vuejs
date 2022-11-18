a787db7777c7787d3ca8a0ab7980bf9937043838
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-18 15:39:47 +0100

    added import fontawesome

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
+/* Set up using Vue 3 */
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import {
+    faFacebook,
+    faTwitter,
+    faInstagram,
+    faYoutube
+} from '@fortawesome/free-brands-svg-icons'
+
+import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faFacebook, faTwitter, faInstagram, faYoutube, faPhoneFlip)
+
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
