543e83fa0860e4fcc0acde4695ba8757cbd0fcb1
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-21 12:36:37 +0100

    section services with cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,7 +3,28 @@ export default {
     name: "AppMain",
     data() {
         return {
-
+            services: [
+                {
+                    image: 'avada-movers-serviceonephoto-final-400x255.jpg',
+                    title: "Two Man Teams",
+                    text: "Lorem ipsum dolor amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+                },
+                {
+                    image: 'avada-movers-servicetwophoto-final-400x255.jpg',
+                    title: "We Do All The Lifting",
+                    text: "Lorem ipsum dolor amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+                },
+                {
+                    image: 'avada-movers-servicethreephoto-final-400x255.jpg',
+                    title: "Coast To Coast",
+                    text: "Lorem ipsum dolor amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+                }
+            ]
+        }
+    },
+    methods: {
+        getImageUrl(url) {
+            return new URL(url, import.meta.url).href;
         }
     }
 }
@@ -32,9 +53,25 @@ export default {
                     <h6>WE ARE A PROFESSIONAL</h6>
                     <h2>Full-Service Solution</h2>
                 </div>
+                <div class="row row-cols-3 mt-5">
+                    <div class="col p-0" v-for="(service, index) in services" :key="index">
+                        <div class="card">
+                            <img :src="getImageUrl(`../assets/images/${service.image}`)" alt="">
+                            <!-- <img src="../assets/images/avada-movers-serviceonephoto-final-400x255.jpg" alt=""> -->
+                            <div class="text-center p-4">
+                                <h5>{{ service.title }}</h5>
+                                <p>{{ service.text }}</p>
+                                <button class="btn">READ MORE</button>
+                            </div>
 
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
+        <div class="big-banner">
+
+        </div>
 
     </main>
 </template>
@@ -66,25 +103,36 @@ export default {
             font-size: .8rem;
         }
     }
-
-    .btn {
-        width: 220px;
-        height: 40px;
-        font-size: .8rem;
-        font-weight: 700;
-        border-radius: 20px;
-        color: white;
-        background-color: $denim;
-        padding-left: 1em;
-        padding-right: 1em;
-        margin-top: 1.5em;
-    }
 }
 
-.services {
-    .card {
-        width: 300px;
-        height: 400px;
+.btn {
+    max-width: 220px;
+    height: 40px;
+    font-size: .8rem;
+    font-weight: 700;
+    border-radius: 20px;
+    color: white;
+    background-color: $denim;
+    padding-left: 1em;
+    padding-right: 1em;
+}
+
+.card {
+    width: 300px;
+    border-radius: 0;
+    border: 15px solid #F7F8FC;
+
+    .text-center {
+        border-top: 15px solid #F7F8FC;
+
+        h5 {
+            color: $denim;
+            font-weight: 700;
+        }
+
+        p {
+            font-size: .7rem;
+        }
     }
 }
 </style>
