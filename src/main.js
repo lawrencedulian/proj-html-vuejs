8ecdb378f25cfc66cfcf736a9abc73e74c9f418e
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-22 17:16:07 +0100

    removed fortawesome import

--- src/main.js
+++ src/main.js
@@ -2,24 +2,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-import {
-    faFacebook,
-    faTwitter,
-    faInstagram,
-    faYoutube
-} from '@fortawesome/free-brands-svg-icons'
-
-import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faFacebook, faTwitter, faInstagram, faYoutube, faPhoneFlip)
-
-
 createApp(App)
-    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
