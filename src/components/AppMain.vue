eb4783e0e83b78ca5083a445ecfb4b3a8a410588
lawrencedulian <lawrenceedulian@gmail.com>
2022-11-22 17:18:07 +0100

    done main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,25 +1,14 @@
 <script>
+import { store } from '../store';
+import AppButton from './AppButton.vue';
 export default {
     name: "AppMain",
+    components: {
+        AppButton
+    },
     data() {
         return {
-            services: [
-                {
-                    image: 'avada-movers-serviceonephoto-final-400x255.jpg',
-                    title: "Two Man Teams",
-                    text: "Lorem ipsum dolor amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-                },
-                {
-                    image: 'avada-movers-servicetwophoto-final-400x255.jpg',
-                    title: "We Do All The Lifting",
-                    text: "Lorem ipsum dolor amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-                },
-                {
-                    image: 'avada-movers-servicethreephoto-final-400x255.jpg',
-                    title: "Coast To Coast",
-                    text: "Lorem ipsum dolor amet, consectetur adipiscing elit. Nullam ac consectetur augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-                }
-            ]
+            store
         }
     },
     methods: {
@@ -33,36 +22,35 @@ export default {
 <template>
     <main>
         <div class="small-banner text-center">
-            <h5>GET A FREE QUOTE ONLINE NOW!</h5>
+            <h6><i class="fa-regular fa-paper-plane me-2"></i>GET A FREE QUOTE ONLINE NOW!</h6>
         </div>
         <section class="award">
             <div class="container-fluid text-center">
-                <h6 class="mb-2">HEADACHE FREE</h6>
+                <h5 class="mb-2">HEADACHE FREE</h5>
                 <h2>Award Winning</h2>
                 <h2>Moving Company</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore atque impedit velit ipsum ad
                     laudantium
                     exercitationem mollitia maxime, id quasi quod quis enim, aliquam voluptatem, doloribus dolorem. Id,
                     delectus iusto.</p>
-                <button class="btn">READ OUR TESTIMONIALS</button>
+                <AppButton name="READ OUT TESTIMONIALS" />
             </div>
         </section>
         <section class="services">
             <div class="container">
                 <div class="text-center">
-                    <h6>WE ARE A PROFESSIONAL</h6>
+                    <h5>WE ARE A PROFESSIONAL</h5>
                     <h2>Full-Service Solution</h2>
                 </div>
                 <div class="row row-cols-3 mt-5">
-                    <div class="col p-0 d-flex justify-content-center" v-for="(service, index) in services"
+                    <div class="col p-0 d-flex justify-content-center" v-for="(service, index) in store.services"
                         :key="index">
                         <div class="card">
                             <img :src="getImageUrl(`../assets/images/${service.image}`)" alt="">
-                            <!-- <img src="../assets/images/avada-movers-serviceonephoto-final-400x255.jpg" alt=""> -->
                             <div class="text-center p-4">
                                 <h5>{{ service.title }}</h5>
                                 <p>{{ service.text }}</p>
-                                <button class="btn">READ MORE</button>
+                                <AppButton name="READ MORE" />
                             </div>
                         </div>
                     </div>
@@ -73,15 +61,12 @@ export default {
             <div class="container">
                 <div class="card p-4">
                     <div class="text-center ">
-                        <h6>WE REMOVE THE HEADACHE FROM MOVING</h6>
+                        <h5>WE REMOVE THE HEADACHE FROM MOVING</h5>
                         <h2>Move With Ease</h2>
                         <p>With our super simple service we can take you from getting a quote, right through to being in
-                            you
-                            new
-                            home with all of your belongings securely packed and safely delivered, even in the rooms of
-                            your
-                            choice.</p>
-                        <button class="btn">GET A FREE QUOTE NOW</button>
+                            you new home with all of your belongings securely packed and safely delivered, even in the
+                            rooms of your choice.</p>
+                        <AppButton name="GET A FREE QUOTE NOW" />
                     </div>
                 </div>
             </div>
@@ -89,53 +74,25 @@ export default {
         <section class="testimonials">
             <div class="container">
                 <div class="text-center">
-                    <h6>HAPPY CUSTOMERS</h6>
+                    <h5>HAPPY CUSTOMERS</h5>
                     <h2>TESTIMONIALS</h2>
                 </div>
+
                 <div class="row row-cols-3 mt-5">
-                    <div class="col p-0 d-flex justify-content-center">
+                    <div class="col p-0 d-flex justify-content-center"
+                        v-for="(testimonial, index) in store.testimonials" :key="index">
                         <div class="card">
-                            <img src="../assets/images/avada-movers-johndoe-final-200x200.jpg" alt="">
-                            <!-- <img src="../assets/images/avada-movers-serviceonephoto-final-400x255.jpg" alt=""> -->
+                            <img :src="getImageUrl(`../assets/images/${testimonial.image}`)" alt="">
                             <div class="text-center p-4">
-                                <h5>John Doe</h5>
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus dolor,
-                                    aliquet sed maximus et, vestibulum pulvinar leo. Vivamus varius pretium fermentum."
-                                </p>
-
+                                <h5>{{ testimonial.name }}</h5>
+                                <p> {{ testimonial.text }} </p>
                             </div>
                         </div>
                     </div>
-                    <div class="col p-0 d-flex justify-content-center">
-                        <div class="card">
-                            <img src="../assets/images/avada-movers-janedoe-final-200x200.jpg" alt="">
-                            <!-- <img src="../assets/images/avada-movers-serviceonephoto-final-400x255.jpg" alt=""> -->
-                            <div class="text-center p-4">
-                                <h5>Jane Doe</h5>
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus dolor,
-                                    aliquet sed maximus et, vestibulum pulvinar leo. Vivamus varius pretium fermentum."
-                                </p>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col p-0 d-flex justify-content-center">
-                        <div class="card">
-                            <img src="../assets/images/avada-movers-johnsmith-final-200x200.jpg" alt="">
-                            <!-- <img src="../assets/images/avada-movers-serviceonephoto-final-400x255.jpg" alt=""> -->
-                            <div class="text-center p-4">
-                                <h5>John Smith</h5>
-                                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus dolor,
-                                    aliquet sed maximus et, vestibulum pulvinar leo. Vivamus varius pretium fermentum."
-                                </p>
-
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
-                <div class="d-flex justify-content-center"><button class="btn mt-3">READ MORE TESTIMONIALS</button>
-                </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <AppButton class="mt-3" name="READ MORE TESTIMONIALS" />
             </div>
         </section>
         <section class="request-quote">
@@ -143,7 +100,7 @@ export default {
                 <h2>Request A Free Quote</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nullam ac consectetur argue. Orci varius
                     natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <button class="btn">CLICK HERE</button>
+                <AppButton name="CLICK HERE" />
             </div>
         </section>
     </main>
@@ -151,14 +108,14 @@ export default {
 
 <style lang="scss" scoped>
 @use "../styles/partials/palette" as *;
+@use "../styles/partials/variables" as *;
 
 .small-banner {
     background-color: $denim-vibrant;
     padding: 2em;
 
-    h5 {
+    h6 {
         margin: 0;
-        font-size: .8rem;
         font-weight: 400;
         color: white;
     }
@@ -171,51 +128,33 @@ export default {
         width: 50%;
         padding: 5em;
 
+        h5 {
+            color: $dove-gray;
+        }
+
         p {
             margin-top: 1.5em;
-            font-size: .8rem;
         }
     }
-}
-
-.card {
-    width: 300px;
-    border-radius: 0;
-}
-
-.btn {
-    max-width: 220px;
-    height: 40px;
-    font-size: .8rem;
-    font-weight: 700;
-    border-radius: 20px;
-    color: white;
-    background-color: $denim;
-    padding-left: 1em;
-    padding-right: 1em;
 }
 
 .services {
     padding: 5em;
 
     .card {
+        margin: 2em;
         border: 15px solid #F7F8FC;
     }
 
     .text-center {
         h5 {
             color: $denim;
-            font-weight: 700;
-        }
-
-        p {
-            font-size: .7rem;
         }
     }
 }
 
 .big-banner {
-    height: 500px;
+    height: $big-banner-height;
     background-image:
         linear-gradient(to right, rgba(0, 0, 0, 0), rgba(219, 234, 253, 1)),
         url(../assets/images/avada-movers-servicetwophoto-final.jpg);
@@ -223,40 +162,53 @@ export default {
     background-position: center;
 
     .card {
-        width: 450px;
         border: 15px solid $lima-dark;
         background-color: $lima;
 
         position: absolute;
         top: 20%;
         right: 0px;
-        transform: translateX(-220px);
+        transform: translateX(-240px);
 
         .text-center {
             color: white;
             font-size: .8rem;
 
-            h6 {
+            h2,
+            h5 {
                 color: white;
             }
 
-            h2 {
-                color: white;
+            h5 {
+                font-size: .6rem;
             }
         }
     }
 }
 
 .testimonials {
-    height: 700px;
+    height: $testimonials-height;
     padding: 5em;
     background-image: url(../assets/images/avada-movers-testimonial-background.jpg);
     background-size: cover;
 
-    img {
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
+    .card {
+        display: flex;
+        align-items: center;
+        border: none;
+        background: none;
+
+        img {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+        }
+
+        .text-center {
+            h5 {
+                color: $denim;
+            }
+        }
     }
 }
 
@@ -271,7 +223,6 @@ export default {
 
         p {
             margin-top: 1.5em;
-            font-size: .8rem;
         }
     }
 }
